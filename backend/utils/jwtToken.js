1bad7bcf51831d0d8056fe22da8975c400818788
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-07-13 17:30:59 +0530

    generate Driver's token

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,8 +1,20 @@
 const generateToken = (user, message, statusCode, res) => {
+    let cookieName;
+    // Determine the cookie name based on the user role
+    if (user.role === "Admin") {
+        cookieName = "adminToken";
+    } else if (user.role === "Driver") {
+        cookieName = "driverToken";
+    } else {
+        cookieName = "userToken";
+    }
+
+    // Generate the token
     const token = user.generateJsonWebToken();
-    const cookieName = user.role === "Admin" ? "adminToken" : "userToken" ;
+    
+    // Set the cookie with the appropriate name and send the response
     res.status(statusCode).cookie(cookieName, token, {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60* 1000),
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
     }).json({
         success: true,
@@ -13,4 +25,5 @@ const generateToken = (user, message, statusCode, res) => {
 };
 
 module.exports = { generateToken };
+
 
